bf93571371e75e4aa03091f4bd18f250c644d539
Norayr <asoyannorayr99@gmail.com>
2023-10-17 20:13:51 +0300

    add

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+    public char charExpression ( int a){
+            int s = '\\';
+            int sum = s + a;
+            char unicode = (char) sum;
+            return unicode;
+        }
+    }
